97b6fa9cd78e6d7ac3707fee5b811338492518e1
Cauê Gualberto <caue.gualberto00@gmail.com>
2021-09-17 01:05:16 -0300

    cores aleatórias e gera cpf

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("oi");
+import generateColor from "randomColor";
+import cpfGen from "@lacussoft/cpf-gen";
+
+let h1CPF = document.getElementById("textCPF");
+let buttonCPF = document.getElementById("buttonGenCPF");
+
+document.body.style.backgroundColor = generateColor();
+h1CPF.innerHTML = cpfGen({ format: true });
+
+function newCPF() {
+  h1CPF.innerHTML = cpfGen({ format: true });
+}
+
+buttonCPF.addEventListener("click", newCPF);
